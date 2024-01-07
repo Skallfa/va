PixelExperience/vendor_aosp

config: qcom: Enable some condititions based on kernel version
[1] - Bengal-5.15 is not part of 5_15_FAMILY and TARGET_BOARD_PLATFORM conflicts with 4.19, so allow conditions to be enabled when checking kernel version.

Change-Id: I7e0756dcda7d681f4b85afa1c99f5ba481c3fb70
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

config/BoardConfigQcom.mk
@@ -126,12 +126,12 @@ ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM
 endif
 
 # Enable Gralloc4 on UM platforms that support it
-ifneq ($(filter $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
 # Select AR variant of A-HAL dependencies
-ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     TARGET_USES_QCOM_AUDIO_AR ?= true
 endif
 
@@ -157,12 +157,12 @@ endif
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY)
 
 # Opt-in for old rmnet_data driver
-ifeq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.15, $(TARGET_KERNEL_VERSION)))
     SOONG_CONFIG_rmnetctl_old_rmnet_data := true
 endif
 
 # Use full QTI gralloc struct for GKI 2.0 targets
-ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= true
     TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= true
 endif

Skallfa/va

config/BoardConfigQcom.mk
@@ -122,12 +122,12 @@ ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM
 endif
 
 # Enable Gralloc4 on UM platforms that support it
-ifneq ($(filter $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
 # Select AR variant of A-HAL dependencies
-ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     TARGET_USES_QCOM_AUDIO_AR ?= true
 endif
 
@@ -153,12 +153,12 @@ endif
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY)
 
 # Opt-in for old rmnet_data driver
-ifeq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifeq (,$(filter 5.15, $(TARGET_KERNEL_VERSION)))
     SOONG_CONFIG_rmnetctl_old_rmnet_data := true
 endif
 
 # Use full QTI gralloc struct for GKI 2.0 targets
-ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
     TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= true
     TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= true
 endif